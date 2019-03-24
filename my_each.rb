0dc213ceb737fc949f6557b84c68a06cc4d7b8d4
Kane Koo <kanekoo@gmail.com>
2019-03-24 22:35:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each (collecshun)
+  i=0
+  clength=collecshun.length
+  if clength > 0 
+  while i < clength 
+     yield(collecshun[i])
+     i += 1
+    end
+  end
+  collecshun
   # code here
 end
